21d32b2c6db52ace41902cf430633625a50820db
BhagwanSingh3035 <bhagwan@joinnextmed.com>
2023-06-14 17:29:14 +0530

    backend - admin model registered to django-admin panel

--- adminapp/admin.py
+++ adminapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import CustomUser, Department, Category
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'create_by', 'create_date', 'update_by', 'update_date')
+    search_fields = ('name', )
+    ordering = ('update_date',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'create_by', 'create_date', 'update_by', 'update_date')
+    search_fields = ('name', )
+    ordering = ('update_date', )
+
+
 # Register your models here.
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Category, CategoryAdmin)
