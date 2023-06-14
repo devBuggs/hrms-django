120dab48c8bb9685f2ca9b26b5e65aaff070fba9
BhagwanSingh3035 <bhagwan@joinnextmed.com>
2023-06-14 16:05:23 +0530

    backend - user model created

--- adminapp/models.py
+++ adminapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+from .managers import CustomUserManager
+
+
+
+
 
 # Create your models here.
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+    
