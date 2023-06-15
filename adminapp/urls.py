84dc210372fa4c269dd570fede46dad40d92a4d7
BhagwanSingh3035 <bhagwan@joinnextmed.com>
2023-06-15 16:10:51 +0530

    BACKEND - auth urls fixed

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'adminapp'
 
 urlpatterns = [
-    path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('login', views.login_view, name='login'),
+    path('logout', views.logout_view, name='logout'),
+    path('forget-password', views.forget_password_view, name="forget-password"),
 ]
