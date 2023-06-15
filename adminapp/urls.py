52e4500bc7225baa7d17ccf5bf6f52cdbb3b4cc5
BhagwanSingh3035 <bhagwan@joinnextmed.com>
2023-06-15 10:48:47 +0530

    BACKEND - admin url updated

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'adminapp'
 
 urlpatterns = [
-    # TODO: webapp urls
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
