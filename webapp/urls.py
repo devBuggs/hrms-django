8ab4b8310c77604aba3f1fc58e37553637fff611
BhagwanSingh3035 <bhagwan@joinnextmed.com>
2023-06-16 14:52:49 +0530

    BACKEND - dashboard pages added

--- webapp/urls.py
+++ webapp/urls.py
@@ -6,7 +6,13 @@ app_name = 'webapp'
 
 urlpatterns = [
     # TODO: webapp urls
-    path('', views.index, name='home'),
+    path('', views.index_view, name='index'),
+    path('profile/', views.profile_view, name='profile'),
+    path('employee/', views.employee_view, name='employee'),
+    path('leave/', views.leave_view, name='leave'),
+    path('attendance/', views.attendance_view, name='attendance'),
+    path('calender/', views.calender_view, name='calender'),
+    path('todo/', views.todo_view, name='todo'),
 
     # Matches any html file
     # re_path(r'^.*\.*', views.pages, name='pages'),
--- webapp/views.py
+++ webapp/views.py
@@ -1,20 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django import template
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 
-# Create your views here.
-@login_required(login_url="adminapp:login")
-def index(request):
-    context = {'segment': 'index'}
-
-    html_template = loader.get_template('home/index.html')
-    return HttpResponse(html_template.render(context, request))
-
 
 # @login_required(login_url="adminapp:login")
+def index_view(request):
+    context = {'segment': 'index'}
+    return render(request, 'home/index.html', context)
+
+
+# Create your views here.
+# @login_required(login_url="adminapp:login")
+# def index(request):
+#     context = {'segment': 'index'}
+
+#     html_template = loader.get_template('home/index.html')
+#     # return HttpResponse(html_template.render(context, request))
+#     return render(request, html_template, {})
+
+
+@login_required(login_url="adminapp:login")
 def pages(request):
     context = {}
     # All resource paths end in .html.
@@ -38,3 +46,44 @@ def pages(request):
     except:
         html_template = loader.get_template('home/page-500.html')
         return HttpResponse(html_template.render(context, request))
+
+
+
+
+
+
+
+
+
+
+
+def profile_view(request):
+    context = {'segment': 'profile'}
+    return render(request, 'home/page-user.html', context)
+
+
+def employee_view(request):
+    context = {'segment': 'employee'}
+    return render(request, 'home/employee.html', context)
+
+
+def leave_view(request):
+    context = {'segment': 'leave'}
+    return render(request, 'home/leave.html', context)
+
+
+def attendance_view(request):
+    context = {'segment': 'attendance'}
+    return render(request, 'home/attendance.html', context)
+
+
+
+def calender_view(request):
+    context = {'segment': 'calender'}
+    return render(request, 'home/calender.html', context)
+
+
+
+def todo_view(request):
+    context = {'segment': 'todo'}
+    return render(request, 'home/todo.html', context)
